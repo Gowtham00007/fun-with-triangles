c41474e06254cc458e427049669ba01d1c9751e2
Gowtham <gowthamnayak365@gmail.com>
2023-09-08 21:12:51 +0530

    update: updating features

---
+++ main4.js
@@ -0,0 +1,16 @@
+const sides = document.querySelectorAll('.side');
+const calculateAreaBtn = document.querySelector('#calculate-area-btn');
+const output =document.querySelector('#output');
+
+function calculateArea(a,b){
+    const areaOfTriangles = 1/2*(a*b) ;
+    return areaOfTriangles;
+}
+
+function triangleArea(){
+    const areaOfTriangles = calculateArea(Number(sides[0].value),Number(sides[1].value));
+    output.innerText=` the area of the triangle is ${areaOfTriangles}units`;
+
+}
+
+calculateAreaBtn.addEventListener('click',triangleArea);
